d2b50039c373c6853b1571b52b827b413ed68178
Katiana-jpg <60975303+Katiana-jpg@users.noreply.github.com>
2020-02-19 09:47:11 -0500

    user
    user

---
+++ lib/utilisateur.dart
@@ -0,0 +1,28 @@
+class Utilisateur {
+  String prenom;
+
+  String nomFamille;
+
+  int age;
+
+  double poids;
+
+  double taille;
+
+  double imc;
+
+  Utilisateur(
+      String prenom, String nomFamille, int age, double poids, double taille) {
+    this.prenom = prenom;
+
+    this.nomFamille = nomFamille;
+
+    this.age = age;
+
+    this.poids = poids;
+
+    this.taille = taille;
+
+    this.imc = poids / (taille * taille);
+  }
+}
