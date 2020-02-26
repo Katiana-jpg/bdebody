a9e02d3a8a730b5f2deb8586d5184202dcfbc6b0
Richard <1856039@bdeb.qc.ca>
2020-02-26 12:59:39 -0500

    Update calendrier.dart

--- lib/menu/calendrier.dart
+++ lib/menu/calendrier.dart
@@ -8,72 +8,89 @@ class Calendrier extends StatefulWidget {
 }
 
 class CalendrierState extends State<Calendrier> {
-  List <String> annee = ['2020','2021','2022',];
-  String choixannee ;
-  List <String> mois = ['0','1','2','3','4'];
-  String choixmois ;
-  List <String> jours = ['0','1','2',];
-  String choixjours ;
+  List<String> annee = ['Année',
+    '2020',
+    '2021',
+    '2022',
+  ];
+  String choixannee = 'Mois';
+  List<String> mois = ['Mois','0', '1', '2', '3', '4'];
+  String choixmois  ;
+  List<String> jours = [
+    'Jours',
+    '0',
+    '1',
+    '2',
+  ];
+  String choixjours;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          
-          child: Row(
-            children: <Widget>[
-              SizedBox(width :30.0),
-              Text('Année',
-              style: TextStyle(
-                color:Colors.yellowAccent,
-              ),),
-              SizedBox(width :20.0),
-              DropdownButton <String>(
-                value:choixannee,
-                
-                items: annee.map((String choix){
-                  return DropdownMenuItem<String>(
-                    value: choix,
-                    child: Text(choix));
-                }).toList(),
-                 onChanged:(String nouveau) {
-                   setState(() {
-                     this.choixannee = nouveau;
-                   });},
-              ),Text('mois',
-              style: TextStyle(
-                color:Colors.yellowAccent,
-              ),),
-              SizedBox(width :20.0),
-              DropdownButton <String>(
-                value:choixmois,
-                items: mois.map((String choix){
-                  return DropdownMenuItem<String>(
-                    value: choix,
-                    child: Text(choix));
-                }).toList(),
-                 onChanged:(String nouveau) {
-                   setState(() {
-                     this.choixmois = nouveau;
-                   });},
-              ),Text('jour',
-              style: TextStyle(
-                color:Colors.yellowAccent,
-              ),),
-              SizedBox(width :20.0),
-              DropdownButton <String>(
-                value:choixjours,
-                items: jours.map((String choix){
-                  return DropdownMenuItem<String>(
-                    value: choix,
-                    child: Text(choix));
-                }).toList(),
-                 onChanged:(String nouveau) {
-                   setState(() {
-                     this.choixjours = nouveau;
-                   });},
-              ),
-            ],
+      child: Row(
+        children: <Widget>[
+          Text(
+            'Choix de la date :',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
-        ));
-}
+          SizedBox(width: 19.0),
+          DropdownButton<String>(
+            style: TextStyle(color: Colors.deepPurple, fontSize: 15.0),
+            underline: Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            value: annee.elementAt(0),
+            iconSize: 24,
+            elevation: 24,
+            items: annee.map((String choix) {
+              return DropdownMenuItem<String>(value: choix, child: Text(choix));
+            }).toList(),
+            onChanged: (String nouveau) {
+              setState(() {
+                this.choixannee = nouveau;
+              });
+            },
+          ),
+          SizedBox(width: 19.0),
+          DropdownButton<String>(
+            style: TextStyle(color: Colors.deepPurple, fontSize: 15.0),
+            underline: Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            value: mois.elementAt(0),
+            items: mois.map((String choix) {
+              return DropdownMenuItem<String>(value: choix, child: Text(choix));
+            }).toList(),
+            onChanged: (String nouveau) {
+              setState(() {
+                this.choixmois = nouveau;
+              });
+            },
+          ),
+          
+          SizedBox(width: 19.0),
+          DropdownButton<String>(
+            style: TextStyle(color: Colors.deepPurple, fontSize: 15.0),
+            underline: Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            value:jours.elementAt(0),
+            items: jours.map((String choix) {
+              return DropdownMenuItem<String>(value: choix, child: Text(choix));
+            }).toList(),
+            onChanged: (String nouveau) {
+              setState(() {
+                this.choixjours = nouveau;
+              });
+            },
+          ),
+        ],
+      ),
+    ));
+  }
 }
