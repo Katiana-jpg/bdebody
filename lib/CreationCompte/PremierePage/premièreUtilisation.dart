6fa1e9b6bc1f86a3ad943146b20a15aa424b94f6
BaeKarl <60982041+BaeKarl@users.noreply.github.com>
2020-03-12 16:20:01 -0400

    suite premiere utilisation

--- lib/CreationCompte/PremierePage/premièreUtilisation.dart
+++ lib/CreationCompte/PremierePage/premièreUtilisation.dart
@@ -2,10 +2,11 @@
 
 import 'package:bdebody/utilisateur.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 
 import 'sexe.dart';
-import 'dateDeNaissance.dart';
+
 
 class PremiereUtilisation extends StatefulWidget {
   PremiereUtilisation({Key key}) : super(key: key);
@@ -24,6 +25,10 @@ class PremiereUtilisationState extends State<PremiereUtilisation> {
   String _courriel;
   String _motDePasse;
   String _imc;
+  DateTime dateNaissance;
+  int dateNaissanceMois;
+  int dateNaissanceAnnee;
+  int dateNaissanceJour;
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +90,46 @@ class PremiereUtilisationState extends State<PremiereUtilisation> {
         new SizedBox(
           height: 10.0,
         ),
-        DateDeNaissance(),
+        Row(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           children: <Widget>[
+           FlatButton(
+             padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+    onPressed: () {
+        DatePicker.showDatePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(1900, 1, 1),
+                              maxTime: DateTime(2020, 3, 9), onChanged: (date) {
+                            print('change $date');
+                          }, onConfirm: (date) {
+                            setState(() {
+                               dateNaissanceMois=date.month;
+                               dateNaissance=date;
+                               dateNaissanceAnnee=date.year;
+                               dateNaissanceJour=date.day;
+                            print('confirm $date');
+                            });
+                           
+                          }, currentTime: DateTime.now(), locale: LocaleType.fr);
+                          
+                          
+    },
+    child: Text(
+      'Date de naissance',
+      textAlign :TextAlign.left,
+        
+        style: TextStyle(color: Colors.blue),
+    )),
+    
+    Container(
+      
+   child: Text(
+(dateNaissance==null)? '': dateNaissanceAnnee/dateNaissanceMois/dateNaissanceJour,
+
+
+    ),
+    ), 
+             ]  ),
         new SizedBox(
           height: 10.0,
         ),
@@ -184,7 +228,7 @@ class PremiereUtilisationState extends State<PremiereUtilisation> {
      
       _formKey.currentState.save();
       _imc=( (double.parse(_poids) / (double.parse(_taille)/100 * double.parse(_taille)/100)).abs()).toStringAsFixed(2);
-       Utilisateur utilisateur = Utilisateur( nom: _name,age:'10',courriel: _courriel,motDePasse: _motDePasse,poids: _poids,taille: _taille,imc:_imc);
+       Utilisateur utilisateur = Utilisateur( nom: _name,age:calculateAge(dateNaissance),courriel: _courriel,motDePasse: _motDePasse,poids: _poids,taille: _taille,imc:_imc);
       Navigator.pushReplacementNamed(context, '/menuProfil',arguments: {                               
                 'nom': utilisateur.nom,'age':utilisateur.age,'courriel': utilisateur.courriel,'motDePasse': utilisateur.motDePasse,'poids':utilisateur.poids,'taille':utilisateur.taille,'imc':utilisateur.imc
       },);
@@ -198,6 +242,21 @@ class PremiereUtilisationState extends State<PremiereUtilisation> {
       });
     }
   }
-
+calculateAge(DateTime birthDate) {
+  DateTime currentDate = DateTime.now();
+  int age = currentDate.year - birthDate.year;
+  int month1 = currentDate.month;
+  int month2 = birthDate.month;
+  if (month2 > month1) {
+    age--;
+  } else if (month1 == month2) {
+    int day1 = currentDate.day;
+    int day2 = birthDate.day;
+    if (day2 > day1) {
+      age--;
+    }
+  }
+  return age.toString();
+}
   
 }
