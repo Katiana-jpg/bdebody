5dc2b4375ce05a5d93b30c245e5487b0248894d0
BaeKarl <60982041+BaeKarl@users.noreply.github.com>
2020-03-24 12:12:40 -0400

    disposuite

--- lib/CreationCompte/PremierePage/disponibilité.dart
+++ lib/CreationCompte/PremierePage/disponibilité.dart
@@ -6,8 +6,34 @@ class Disponibilite extends StatefulWidget {
 }
 
 class _DisponibiliteState extends State<Disponibilite> {
+   TimeOfDay depart = TimeOfDay.fromDateTime(DateTime(2020,1,1,0,0) );
+  TimeOfDay fin = TimeOfDay.fromDateTime(DateTime(2020,1,1,0,0)) ; 
+
+  Future<Null> selectTimeDepart(BuildContext context)async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+     initialTime: depart );
+
+if(picked !=  null && picked != depart){
+setState(() {
+  depart = picked;
+});
+}
+  }
+  Future<Null> selectTimeFin(BuildContext context)async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+     initialTime: fin );
+
+if(picked !=  null && picked != fin){
+setState(() {
+  fin = picked;
+});
+}
+  }
   @override
   Widget build(BuildContext context) {
+ 
     return  Scaffold(
       backgroundColor: Colors.yellowAccent,
         body: SafeArea(
@@ -35,15 +61,32 @@ disponibilite('Lundi')
 
  Widget disponibilite (String jour){
 return Row(
+  mainAxisAlignment: MainAxisAlignment.center,
 children: <Widget>[
   Text(jour
   ),
   SizedBox(width:20),
-  Expanded(
-
-  child:TextFormField(
-
-  ))
+  FlatButton.icon(
+    color: Colors.yellowAccent,
+    icon :Icon(Icons.timelapse),
+    label:Text('Début'),
+    onPressed:(){
+selectTimeDepart(context);
+    } ,
+  ),
+  SizedBox(width:20),
+Text(depart.toString().substring(10,15)),
+SizedBox(width:20),
+  FlatButton.icon(
+    color: Colors.yellowAccent,
+    icon :Icon(Icons.timelapse),
+    label:Text('Fin'),
+    onPressed:(){
+selectTimeFin(context);
+    } ,
+  ),
+  SizedBox(width:20),
+Text(fin.toString().substring(10,15)),
 ],
 );
  } 
