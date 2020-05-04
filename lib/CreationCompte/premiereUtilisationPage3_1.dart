f7516f725df1c0157f59e5f7d208d224c7e54980
BaeKarl <60982041+BaeKarl@users.noreply.github.com>
2020-05-04 14:06:45 -0400

    dartdoc pup3_1

--- lib/CreationCompte/premiereUtilisationPage3_1.dart
+++ lib/CreationCompte/premiereUtilisationPage3_1.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 
 class PremiereUtilisationPage3_1 extends StatefulWidget {
   @override
-  _PremiereUtilisationPage3_1State createState() => _PremiereUtilisationPage3_1State();
+  _PremiereUtilisationPage3_1State createState() =>
+      _PremiereUtilisationPage3_1State();
 }
 
-class _PremiereUtilisationPage3_1State extends State<PremiereUtilisationPage3_1> {
+class _PremiereUtilisationPage3_1State
+    extends State<PremiereUtilisationPage3_1> {
   List<String> jour = [
     'Lundi',
     'Mardi',
@@ -20,122 +22,108 @@ class _PremiereUtilisationPage3_1State extends State<PremiereUtilisationPage3_1>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[800], body: SafeArea(child: Center(
+        backgroundColor: Colors.grey[800],
+        body: SafeArea(
+            child: Center(
           child: Container(
-          
-          padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-  child: Column (
-    mainAxisAlignment: MainAxisAlignment.center
-    
-    ,
-    children: <Widget>[
-Text(
-  'Disponibilité durant la semaine',
-  style: TextStyle(
-    letterSpacing: 2,
-    fontSize: 20,
-  )
-),
-
-
-  disponibilite(jour.elementAt(0)),
-  disponibilite(jour.elementAt(1)),
- disponibilite(jour.elementAt(2)),
- disponibilite(jour.elementAt(3)),
- disponibilite(jour.elementAt(4)),
- disponibilite(jour.elementAt(5)),
- disponibilite(jour.elementAt(6)),
- Row(
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Disponibilité durant la semaine',
+                      style: TextStyle(
+                        letterSpacing: 2,
+                        fontSize: 20,
+                      )),
+                  disponibilite(jour.elementAt(0)),
+                  disponibilite(jour.elementAt(1)),
+                  disponibilite(jour.elementAt(2)),
+                  disponibilite(jour.elementAt(3)),
+                  disponibilite(jour.elementAt(4)),
+                  disponibilite(jour.elementAt(5)),
+                  disponibilite(jour.elementAt(6)),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        RaisedButton(
+                            child: Text('Retour'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
+                        RaisedButton(
+                            child: Text('Effacer Tout'),
+                            onPressed: () {
+                              setState(() {
+                                utilisateur.disponibiliteSemaine.clear();
+                              });
+                            }),
+                        RaisedButton(
+                            child: Text('Confirmer'),
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                  context, '/premiereUtilisationPage4');
+                            }),
+                      ]),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(50, 100, 50, 0),
+                      height: 400,
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          RaisedButton(
-                              child: Text('Retour'),
-                              onPressed: () {
-                                Navigator.pop(context);}),
-                          RaisedButton(
-                              child: Text('Reset'),
-                              onPressed: () {
-                               setState(() {
-                                 utilisateur.disponibiliteSemaine.clear();   
-                               });
-                                
- 
-                                
-                               
-                              }),      
-                          RaisedButton(
-                              child: Text('Confirmer'),
-                              onPressed: () {
-                               
-                                   
- 
-                                Navigator.pushReplacementNamed(
-                                    context, '/premiereUtilisationPage4');
-                               
-                              }),
-                        ]),
- Container(
-                     padding: EdgeInsets.fromLTRB(50, 100, 50, 0),
-                     height: 400,
-                     
-                     child:Row(  mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
-                       getTextWidgets(utilisateur.disponibiliteSemaine),
-getTextWidgets2(utilisateur.disponibiliteSemaine),
-getTextWidgets3(utilisateur.disponibiliteSemaine),
-                     ], )
-                       
-                    ) 
-    ],
-  )
-),
+                          jourDeLaDisponibilite(utilisateur.disponibiliteSemaine),
+                          debutDeLaDisponibilite(utilisateur.disponibiliteSemaine),
+                          finDeLaDisponibilite(utilisateur.disponibiliteSemaine),
+                        ],
+                      ))
+                ],
+              )),
         )));
   }
- Widget getTextWidgets(List<HeureDisponible> strings)
-  {
+///Affiche le jour de chaque élément de la liste des disponibilités du client [disponibiliteSemaine]
+  Widget jourDeLaDisponibilite(List<HeureDisponible> disponibiliteSemaine) {
     List<Widget> list = new List<Widget>();
-    for(var i = 0; i < strings.length; i++){
-        list.add(new Text(strings.elementAt(i).jour));
-           }
-    return new Column( children: list);
-  } 
-   Widget getTextWidgets2(List<HeureDisponible> strings)
-  {
+    for (var i = 0; i < disponibiliteSemaine.length; i++) {
+      list.add(new Text(disponibiliteSemaine.elementAt(i).jour));
+    }
+    return new Column(children: list);
+  }
+
+///Affiche le début de chaque plage horaire de la liste des disponibilités du client [disponibiliteSemaine]
+  Widget debutDeLaDisponibilite(List<HeureDisponible> disponibiliteSemaine) {
     List<Widget> list = new List<Widget>();
-    for(var i = 0; i < strings.length; i++){
-        list.add(new Text('Début :'+strings.elementAt(i).debut.toString().substring(10,15)));
-           }
-    return new Column( children: list);
-  } 
-   Widget getTextWidgets3(List<HeureDisponible> strings)
-  {
+    for (var i = 0; i < disponibiliteSemaine.length; i++) {
+      list.add(new Text(
+          'Début :' + disponibiliteSemaine.elementAt(i).debut.toString().substring(10, 15)));
+    }
+    return new Column(children: list);
+  }
+///Affiche la fin de chaque plage horaire de la liste des disponibilités du client [disponibiliteSemaine]
+  Widget finDeLaDisponibilite(List<HeureDisponible> disponibiliteSemaine) {
     List<Widget> list = new List<Widget>();
-    for(var i = 0; i < strings.length; i++){
-        list.add(new Text('Fin :'+strings.elementAt(i).fin.toString().substring(10,15)));
-           }
-    return new Column( children: list);
-  } 
-   Widget disponibilite (String jour){
-return Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-children: <Widget>[
-  Text(jour
-  ),
-  SizedBox(width:20),
-  IconButton(
-    color: Colors.black,
-    icon :Icon(Icons.control_point),
-    onPressed:(){
+    for (var i = 0; i < disponibiliteSemaine.length; i++) {
+      list.add(new Text(
+          'Fin :' + disponibiliteSemaine.elementAt(i).fin.toString().substring(10, 15)));
+    }
+    return new Column(children: list);
+  }
 
-
-Navigator.pushNamed(context, '/premiereUtilisationPage3_2',arguments: {'jour': jour});
-
-    } ,
-  ),
-  
-
-    
-  
-],
-);
- } 
+/// Une ligne de bouton pour amener le client à choisir la journée [jour] 
+/// où il veut avoir une disponibilité pour faire de l'exercice
+  Widget disponibilite(String jour) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(jour),
+        SizedBox(width: 20),
+        IconButton(
+          color: Colors.black,
+          icon: Icon(Icons.control_point),
+          onPressed: () {
+            Navigator.pushNamed(context, '/premiereUtilisationPage3_2',
+                arguments: {'jour': jour});
+          },
+        ),
+      ],
+    );
+  }
 }
