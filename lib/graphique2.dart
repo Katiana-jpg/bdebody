6e69e03125b01744156ee29212f23f07c8b6990e
MarcAntoineHien <60859573+MarcAntoineHien@users.noreply.github.com>
2020-05-04 01:39:28 -0400

    setState graph

--- lib/graphique2.dart
+++ lib/graphique2.dart
@@ -6,45 +6,43 @@ import 'package:currency_textfield/currency_textfield.dart';
 class Graphique2 extends StatefulWidget {
   //
   Graphique2() : super();
- 
+
   final String title = "Graphique 2";
 
   @override
   Graphique2State createState() => Graphique2State();
 }
- 
+
 class Graphique2State extends State<Graphique2> {
   //
   List<charts.Series> seriesList;
- 
+
   static List<charts.Series<Donnees, DateTime>> _loadData() {
-  
- 
-    final List <Donnees> variationDuPoids=[];
+    final List<Donnees> variationDuPoids = [];
 
-for(int i=0 ;i < utilisateur.listeDate.length;i++){
-variationDuPoids.add(Donnees(utilisateur.listePoids[i],utilisateur.listeDate[i] ));
-
-}
+    for (int i = 0; i < utilisateur.listeDate.length; i++) {
+      variationDuPoids
+          .add(Donnees(utilisateur.listePoids[i], utilisateur.listeDate[i]));
+    }
     return [
       charts.Series<Donnees, DateTime>(
         id: 'Sales',
         domainFn: (Donnees sales, _) => sales.date,
         measureFn: (Donnees sales, _) => sales.poids,
         data: variationDuPoids,
-         colorFn: (_, __) => charts.MaterialPalette.yellow.shadeDefault,
+        colorFn: (_, __) => charts.MaterialPalette.yellow.shadeDefault,
         fillColorFn: (Donnees sales, _) {
           return charts.MaterialPalette.black;
         },
       ),
     ];
   }
- 
-  
-  timeSeries(){
-    return charts.TimeSeriesChart(seriesList,
-        animate: true,
-        primaryMeasureAxis: new charts.NumericAxisSpec(
+
+  timeSeries() {
+    return charts.TimeSeriesChart(
+      seriesList,
+      animate: true,
+      primaryMeasureAxis: new charts.NumericAxisSpec(
           tickProviderSpec: new charts.BasicNumericTickProviderSpec(
               // Make sure we don't have values less than 1 as ticks
               // (ie: counts).
@@ -52,26 +50,20 @@ variationDuPoids.add(Donnees(utilisateur.listePoids[i],utilisateur.listeDate[i] 
               // Fixed tick count to highlight the integer only behavior
               // generating ticks [0, 1, 2, 3, 4].
               desiredTickCount: 5)),
-        
-        defaultRenderer: new charts.LineRendererConfig(includePoints: true),
-         selectionModels: [
+      defaultRenderer: new charts.LineRendererConfig(includePoints: true),
+      selectionModels: [
         charts.SelectionModelConfig(
-          changedListener: (charts.SelectionModel model) {
-            if(model.hasDatumSelection)
-              _onSelectionChanged(model);
-         
-          }
-        )
+            changedListener: (charts.SelectionModel model) {
+          if (model.hasDatumSelection) _onSelectionChanged(model);
+        })
       ],
-         behaviors: [
-
-      new charts.LinePointHighlighter(
-
-        showVerticalFollowLine:
-              charts.LinePointHighlighterFollowLineType.nearest),         
-    ], 
-        );
-   }
+      behaviors: [
+        new charts.LinePointHighlighter(
+            showVerticalFollowLine:
+                charts.LinePointHighlighterFollowLineType.nearest),
+      ],
+    );
+  }
 
   DateTime _time;
   Map<String, num> _measures;
@@ -106,44 +98,51 @@ variationDuPoids.add(Donnees(utilisateur.listePoids[i],utilisateur.listeDate[i] 
 
   @override
   Widget build(BuildContext context) {
-   // CurrencyTextFieldController _controller = CurrencyTextFieldController();
-    var _controller = CurrencyTextFieldController(rightSymbol: "Kg ", decimalSymbol: ".", thousandSymbol: ",");
-   // double val = _controller.doubleValue;
+    // CurrencyTextFieldController _controller = CurrencyTextFieldController();
+    var _controller = CurrencyTextFieldController(
+        rightSymbol: "Kg ", decimalSymbol: ".", thousandSymbol: ",");
+    // double val = _controller.doubleValue;
     var now = new DateTime.now();
 
     // The children consist of a Chart and Text widgets below to hold the info.
     final children = <Widget>[
-      new AppBar(backgroundColor: Colors.amber,title: Text('Statistiques'),),
-    
+      new AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Statistiques'),
+      ),
       Row(
-     
         children: <Widget>[
-             Expanded(
-               flex: 3,
-       child:  TextField(
-        controller: _controller,
-        keyboardType: TextInputType.number,
-  obscureText: false,
-  decoration: InputDecoration(
-      border: OutlineInputBorder(),
-      labelText: 'Ajouter un poids',
-      
-      
-  ),
-), 
-),
- Expanded(
-               flex: 1,
-       child:  FlatButton(onPressed:(){if( _controller.doubleValue!=null){ utilisateur.listePoids.add(_controller.doubleValue);
-        utilisateur.listeDate.add(now);_loadData();}  },
- child: Text('Validé'))
-      )],
-      )
-    ,
+          Expanded(
+            flex: 3,
+            child: TextField(
+              controller: _controller,
+              keyboardType: TextInputType.number,
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Ajouter un poids',
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 1,
+              child: FlatButton(
+                  onPressed: () {
+
+                    if (_controller.doubleValue != null) {
+                      utilisateur.listePoids.add(_controller.doubleValue);
+                      utilisateur.listeDate.add(now);
+                      _loadData();
+                    }
+                    setState(() {
+                      
+                    });
+                  },
+                  child: Text('Validé')))
+        ],
+      ),
       Center(
-        child: new SizedBox(
-            height: 200.0,
-     child:timeSeries()),
+        child: new SizedBox(height: 200.0, child: timeSeries()),
       ),
     ];
 
@@ -151,7 +150,7 @@ variationDuPoids.add(Donnees(utilisateur.listePoids[i],utilisateur.listeDate[i] 
     if (_time != null) {
       children.add(new Padding(
           padding: new EdgeInsets.only(top: 5.0),
-          child: new Text('Date: '+_time.toString().substring(0,10))));
+          child: new Text('Date: ' + _time.toString().substring(0, 10))));
     }
     _measures?.forEach((String series, num value) {
       children.add(new Text('Poids: ${value} Kg'));
@@ -160,18 +159,16 @@ variationDuPoids.add(Donnees(utilisateur.listePoids[i],utilisateur.listeDate[i] 
     return new Column(children: children);
   }
 
-
   @override
   void initState() {
     super.initState();
     seriesList = _loadData();
   }
- 
- }
+}
+
 class Donnees {
   final double poids;
   final DateTime date;
- 
+
   Donnees(this.poids, this.date);
 }
- 
